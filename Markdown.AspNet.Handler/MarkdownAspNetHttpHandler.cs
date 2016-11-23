00e7a0a0acd6b668bb641aabc7a62c581b3ad4aa
Indrajith Rambukkanage <Indrajith Rambukkanage>
2016-11-23 23:25:29 +1100

    Added the markdown handler class project and web project to demonstrate the functionality

---
+++ Markdown.AspNet.Handler/MarkdownAspNetHttpHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Web;
+
+namespace Markdown.AspNet.Handler
+{
+    public class MarkdownAspNetHttpHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var requestedFile = context.Request.PhysicalPath;
+
+            if (string.IsNullOrEmpty(requestedFile) || !File.Exists(requestedFile))
+            {                
+                context.Response.StatusCode = 404;
+                return;
+            }
+
+            var fileContents = File.ReadAllText(requestedFile);
+
+            var markdown = new HeyRed.MarkdownSharp.Markdown();
+            var content = markdown.Transform(fileContents);
+
+            context.Response.Write(content);
+        }
+    }
+}
